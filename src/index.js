f111dfc26f974337afd93bf8a61537e11a1fc9ea
Alex Cline <rev.a.r.cline@gmail.com>
2021-04-21 08:57:40 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 // write your createStore function here
 
+const createStore = (reducer) => {
+  let state;
+
+  const getState = () => state;
+
+  const dispatch = (action) => {
+    state = reducer(state, action);
+    render();
+  };
+
+  return {
+    dispatch,
+    getState,
+  };
+};
+
 function candyReducer(state = [], action) {
   switch (action.type) {
-    case 'ADD_CANDY':
+    case "ADD_CANDY":
       return [...state, action.candy];
     default:
       return state;
@@ -10,13 +26,17 @@ function candyReducer(state = [], action) {
 }
 
 function render() {
-  let container = document.getElementById('container');
-  if(store.getState()) {
-    container.textContent = store.getState().join(' ')
+  let container = document.getElementById("container");
+  if (store.getState()) {
+    container.textContent = store.getState().join(" ");
   } else {
-    throw new Error("the store's state has not been defined yet")
+    throw new Error("the store's state has not been defined yet");
   }
-};
+}
 
 // Use your createStore function and the functions provided here to create a store.
 // Once the store is created, call an initial dispatch.
+
+let store = createStore(candyReducer);
+store.dispatch("@@INIT");
+store.dispatch({ type: "ADD_CANDY", candy: "Milk Duds" });
--- test/index-test.js
+++ test/index-test.js
@@ -1,27 +1,26 @@
-describe('createStore()', () => {
+describe("createStore()", () => {
   let store;
 
-  beforeEach(function(){
+  beforeEach(function () {
     store = createStore(candyReducer);
-  })
+  });
 
-  it('returns an object', function() {
-    expect(store).to.be.an("object")
-  })
+  it("returns an object", function () {
+    expect(store).to.be.an("object");
+  });
 
-  describe('getState method', function(){
-    it('returns the default state based on the reducer for the store', function(){
-      store.dispatch({type: '@@INIT'})
+  describe("getState method", function () {
+    it("returns the default state based on the reducer for the store", function () {
+      store.dispatch({ type: "@@INIT" });
       expect(store.getState()).to.eql([]);
-    })
-  })
+    });
+  });
 
-  describe('dispatch method', function(){
-
-    it('can dispatch actions with data associated to update the state', function(){
-      store.dispatch({type: 'ADD_CANDY', candy: "Jelly Beans"});
+  describe("dispatch method", function () {
+    it("can dispatch actions with data associated to update the state", function () {
+      store.dispatch({ type: "ADD_CANDY", candy: "Jelly Beans" });
       let todos = store.getState();
       expect(todos).to.eql(["Jelly Beans"]);
-    })
-  })
-})
+    });
+  });
+});
